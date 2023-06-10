f8ef4d7d2a717ae95b0094d391253475e9e33521
fruitptr <shaheerahmaddev@gmail.com>
2023-06-10 20:56:11 +0500

    Used Redux and added Message and Loader components

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,22 +1,16 @@
 import {useParams} from 'react-router-dom'
-import {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
 import Rating from '../components/Rating';
-import axios from 'axios';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 
 const ProductScreen = () => {
-const [product, setProduct] = useState([]);
 
     const {id: productId} = useParams();
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const {data} = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        }
-        fetchProduct();
-    }, [productId]);
+    const { data: product, isLoading, error} = useGetProductDetailsQuery(productId);
 
     console.log(productId)
     console.log(product);
@@ -24,7 +18,13 @@ const [product, setProduct] = useState([]);
   return (
     <>
     <Link className='btn btn-light my-3' to='/'>Go Back</Link>
-    <Row>
+
+    { isLoading? (
+        <Loader />
+    ) : error ? (
+        <Message variant='danger'>{error?.data?.message || error.error}</Message>
+    ) : (
+        <Row>
         <Col md={5}>
             <Image src={product.image} alt={product.name} fluid></Image>
         </Col>
@@ -76,6 +76,9 @@ const [product, setProduct] = useState([]);
             </Card>
         </Col>
     </Row>
+    ) }
+
+    
 
 
     </>
