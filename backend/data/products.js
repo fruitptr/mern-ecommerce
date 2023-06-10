61705b124272f76960d3123f06c0ea4c7e9e4566
fruitptr <shaheerahmaddev@gmail.com>
2023-06-10 18:47:33 +0500

    Added Mongo DB functionality

---
+++ backend/data/products.js
@@ -0,0 +1,37 @@
+const products = [
+    {
+        name: 'Nike Slim Shirt Drifit High Quality Discount',
+        image: '/images/imageplaceholder.png',
+        description: 'high quality product',
+        brand: 'Nike',
+        category: 'Shirts',
+        price: 120,
+        countInStock: 0,
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        name: 'Adidas Fit Shirt',
+        image: '/images/imageplaceholder.png',
+        description: 'high quality product',
+        brand: 'Adidas',
+        category: 'Shirts',
+        price: 100,
+        countInStock: 20,
+        rating: 4.0,
+        numReviews: 10,
+    },
+    {
+        name: 'Lacoste Free Shirt',
+        image: '/images/imageplaceholder.png',
+        description: 'high quality product',
+        brand: 'Lacoste',
+        category: 'Shirts',
+        price: 220,
+        countInStock: 0,
+        rating: 4.8,
+        numReviews: 17,
+    },
+]
+
+export default products
