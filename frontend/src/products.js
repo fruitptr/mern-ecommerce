a1e6ea825e150c94a4cf35a23fc619e59e6d4fff
fruitptr <shaheerahmaddev@gmail.com>
2023-06-10 05:03:12 +0500

    Second Commit

---
+++ frontend/src/products.js
@@ -0,0 +1,40 @@
+const products = [
+    {
+        _id: 1,
+        name: 'Nike Slim Shirt',
+        image: '/images/imageplaceholder.png',
+        description: 'high quality product',
+        brand: 'Nike',
+        category: 'Shirts',
+        price: 120,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        _id: 2,
+        name: 'Adidas Fit Shirt',
+        image: '/images/imageplaceholder.png',
+        description: 'high quality product',
+        brand: 'Adidas',
+        category: 'Shirts',
+        price: 100,
+        countInStock: 20,
+        rating: 4.0,
+        numReviews: 10,
+    },
+    {
+        _id: 3,
+        name: 'Lacoste Free Shirt',
+        image: '/images/imageplaceholder.png',
+        description: 'high quality product',
+        brand: 'Lacoste',
+        category: 'Shirts',
+        price: 220,
+        countInStock: 0,
+        rating: 4.8,
+        numReviews: 17,
+    },
+]
+
+export default products
